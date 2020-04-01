45c36c1b7a0b3372a5006b08cd51d551277fce4f
utnaf <davide.fantuzzi@larus-ba.it>
2020-04-01 15:09:03 +0200

    hello world

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+    modules: ["node_modules", path.resolve(__dirname + "/src")],
+    alias: {
+      src: path.resolve(__dirname + "/src")
+    }
+  },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "dist/",
+    filename: "dist/bundle.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    port: 3000,
+    publicPath: "http://localhost:3000/",
+    hotOnly: true
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+};
